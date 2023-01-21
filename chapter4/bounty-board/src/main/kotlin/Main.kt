9981d70a5a3ace7568b90db3f536bfdbb8c2be9c
Dylan Cruz <dylan-cruz@live.com>
2023-01-20 22:24:27 -0500

    chapter4 complete

--- chapter4/bounty-board/src/main/kotlin/Main.kt
+++ chapter4/bounty-board/src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 const val HERO_NAME = "Madrigal"
+var playerLevel = 5
 
 fun main(args: Array<String>) {
     println("The hero announces her presence to the world.")
     println(HERO_NAME)
-    var playerLevel = 4
     println(playerLevel)
 
     val hasBefriendedBarbarians = true
@@ -12,7 +12,22 @@ fun main(args: Array<String>) {
     var canTalkToBarbarians = !hasAngeredBarbarians &&
             (hasBefriendedBarbarians || playerClass == "barbarian")
 
-    val quest = when (playerLevel) {
+    readBountyBoard()
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
+    playerLevel++
+    println(playerLevel)
+    readBountyBoard()
+}
+
+private fun readBountyBoard(canTalkToBarbarians: Boolean = true) {
+    println("The hero approaches the bounty board. It reads:")
+    println(obtainQuest(playerLevel, canTalkToBarbarians))
+}
+
+private fun obtainQuest(playerLevel: Int, canTalkToBarbarians: Boolean): String = when (playerLevel) {
         1 -> "Meet Mr. Bubbles in the land of soft things."
         in 2..5 -> {
             // Check whether diplomacy is an option
@@ -23,17 +38,7 @@ fun main(args: Array<String>) {
             }
         }
         6 -> "Locate the enchanted sword."
-        7 ->  "Recover the long-lost artifact of creation."
+        7 -> "Recover the long-lost artifact of creation."
         8 -> "Defeat Nogartse, bringer of death and eater of worlds."
         else -> "There are no quests right now."
     }
-
-    println("The hero approaches the bounty board. It reads:")
-    println(quest)
-
-    println("Time passes...")
-    println("The hero returns from her quest.")
-
-    playerLevel++
-    println(playerLevel)
-}
